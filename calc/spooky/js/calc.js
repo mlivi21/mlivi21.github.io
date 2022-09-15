9d3346c30903a7464b38983de24c499ff4682c73
mlivi21 <m17.livingston@gmail.com>
2022-09-15 12:32:04 -0400

    calc_sep15

--- calc/spooky/js/calc.js
+++ calc/spooky/js/calc.js
@@ -1,1 +1,18 @@
-console.log() 
+console.log("calc JS loaded :D");
+
+const answer = document.getElementById("result");
+
+
+function testFunction() {
+    answer.innerHTML = "blanched almond";
+}
+
+function testActual() {
+    answer.innerHTML = 144 / 12;
+}
+
+function testFinal() {
+    answer.innerHTML = 6 * 7;
+}
+
+
